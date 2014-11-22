3ad244bdc513fe4b39cc9ffb7e9785107e258c2e
mbhuet <mbhuet@gmail.com>
2014-11-21 22:00:14 -0500

    trees light up when hit

--- Assets/eating.cs
+++ Assets/eating.cs
@@ -40,13 +40,24 @@ public class eating : MonoBehaviour {
 		{
 			score -= 10;
 			if (score < 0) score = 0;
-			for (int i = 0; i< 10; i++) {
+			for (int i = 0; i< 3; i++) {
 				Vector3 pos = new Vector3(transform.position.x+Random.insideUnitCircle.x, transform.position.y+Random.insideUnitCircle.y);
 				pos *= 1;
-				GameObject.Instantiate(bug, pos, Quaternion.identity);
+				Bug b = (GameObject.Instantiate(bug, pos, Quaternion.identity) as GameObject).GetComponent<Bug>();
+				b.StartCoroutine("Show");
 				Debug.Log("Spawn");
+
 			}
+			StartCoroutine("LightTree", other.gameObject);
 			
 		}
+	}
+
+	IEnumerator LightTree(GameObject obj){
+		obj.transform.FindChild ("White").renderer.enabled = false;
+		yield return new WaitForSeconds(.1f);
+		obj.transform.FindChild ("White").renderer.enabled = true;
+
+		
 	}
 }
